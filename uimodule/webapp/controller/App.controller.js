d9ce343074de52238d9cc9b1b8d87b4f96e3b258
Milton Chandradas <milton.chandradas@sap.com>
2020-10-14 01:38:27 -0400

    Complex property binding...

--- uimodule/webapp/controller/App.controller.js
+++ uimodule/webapp/controller/App.controller.js
@@ -1,7 +1,28 @@
-sap.ui.define([
-  "com/sap/ui5demo/controller/BaseController"
-], function(Controller) {
+sap.ui.define(["com/sap/ui5demo/controller/BaseController"], function (
+  Controller
+) {
   "use strict";
 
-  return Controller.extend("com.sap.ui5demo.controller.App", {});
+  return Controller.extend("com.sap.ui5demo.controller.App", {
+    onInit: function () {},
+
+    // onChange update valueState of input
+    onChange: function (oEvent) {
+      let oInput = oEvent.getSource();
+      let validEmail = this._validateInput(oInput.getValue());
+      let sValueState = "None";
+
+      if (!validEmail) {
+        sValueState = "Error";
+      }
+
+      oInput.setValueState(sValueState);
+    },
+
+    // PRIVATE FUNCTIONS
+    _validateInput: function (sValue) {
+      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(String(sValue).toLowerCase());
+    },
+  });
 });
